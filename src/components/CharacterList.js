8bc9d27fb58a73a797544c524309f23fc993f558
chloe alcorn <ca.alcorn@outlook.com>
2018-10-31 10:56:48 +0000

    can render from character list

---
+++ src/components/CharacterList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CharacterList = (props) => {
+
+return(
+    <h1>helo</h1>
+  )
+
+
+}
+
+
+
+export default CharacterList;
--- src/containers/CharacterContainer.js
+++ src/containers/CharacterContainer.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import CharacterList from '../components/CharacterList'
 
 class CharacterContainer extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      characters: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://hp-api.herokuapp.com/api/characters')
+    .then(response => response.json())
+    .then(json => this.setState({characters: json}))
+    .catch(err => console.console.error())
+  }
 
 render(){
   return(
-    <h1>Hello</h1>
+    <div>
+  <CharacterList/>
+
+</div>
   )
 }
 }
